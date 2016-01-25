ab567e19f79598d48b66a3721d0c5f7304bff44a
Amit Kharka <amit.kharka@ge.com>
2016-01-25 13:22:52 +0530

    new changes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var express=require('express');
-var app=express();
-app.use(express.static(__dirname +'/'));
-app.use(express.static(__dirname +'/bower_components'));
-app.use(express.static(__dirname +'/public'));
-app.get('/',function(request,response){
-	response.sendFile("index.html",{root:__dirname});
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(request, response) {
+    response.sendFile("index.html", {
+        root: __dirname
+    });
 })
-app.listen(3031);
+app.listen(3000);
